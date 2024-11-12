8ba84642d37921621625a7e5b9a5e6db033cf566
ArpKevin <arpaskevin11@gmail.com>
2024-11-12 14:20:05 +0100

    dsa

--- index.php
+++ index.php
@@ -4,10 +4,17 @@ include("head.php");
 
 <!doctype html>
 <html lang="en">
-  <?php include("head.php"); ?>
-  <body>
+<?php include("head.php"); ?>
+
+<body>
     <h1>Hello, world!</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+
+    <?php
+    if ($_SERVER['QUERY_STRING'] == "login") include("login.php"); else if ($_SERVER['QUERY_STRING'] == "registration") include("registration.php");
+    ?>
+
     <?php include("footer.php"); ?>
 </body>
+
 </html>
